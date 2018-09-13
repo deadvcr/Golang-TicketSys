7b1df679fee09cd7324baa21df6038e67439bcf8
Matt <admin@resonanceservers.com>
2018-09-13 17:29:48 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -1,38 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
+//Page : every ticket created will follow this structure.
 type Page struct {
-	Title       string
-	Student     string
-	Client      string
-	Description []byte
+	Title string
+	Body  string
 }
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Description, 0600)
+	filename := p.Title + ".json"
+	var page Page
+	json.Unmarshal([]byte(filename), &page)
+	m := Page{p.Title, p.Body}
+	b, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return ioutil.WriteFile("files/"+filename, b, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "files/" + title + ".json"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Description: body}, nil
+	var page Page
+	json.Unmarshal([]byte(body), &page)
+	return &Page{Title: title, Body: page.Body}, nil
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Description: []byte("This is a sample page.")}
-	p1.save()
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	fmt.Println("Starting Listener 🤯")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
@@ -43,10 +59,47 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/view/" {
-		fmt.Fprint(w, "<h1>You didn't specify a page!")
+		fmt.Fprint(w, "<h1>You didn't specify a page!</h1>")
 	} else {
 		title := r.URL.Path[len("/view/"):]
-		p, _ := loadPage(title)
-		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Description)
+		p, err := loadPage(title)
+		if err != nil {
+			http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+			return
+		}
+		t, _ := template.ParseFiles("templates/view.html")
+		t.Execute(w, p)
 	}
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	title = r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("templates/edit.html")
+	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	title = r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: body}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
 }
